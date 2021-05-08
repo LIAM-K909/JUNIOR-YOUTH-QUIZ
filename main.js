a843c6928e08a5f7c3826f4b3f56f301796c140f
LIAM-K909 <83389172+LIAM-K909@users.noreply.github.com>
2021-05-08 14:46:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function submitname()
+{
+    document.getElementById("submitname")
+    window.alert("WOW, GOOD NAME!");
+}
+function subans1()
+{
+    document.getElementById("subans1")
+    window.alert("LOVELY AND A BEAUTIFUL TALENT, DEVELOPE YOUR TALENT!");
+}
+function subans2()
+{
+    document.getElementById("subans2")
+    window.alert("WOW");
+}
+function subans3(){
+    document.getElementById("subans3")
+    window.alert("ATTA BOY! YES");
+}
+function subans4()
+{
+    document.getElementById("subans4")
+    window.alert(" YES YOU ARE RIGHT, ALWAYS DEVELOPE YOUR TALENT!");
+}
+function subans5()
+{
+    document.getElementById("subans5")
+    window.alert("YES, YOU ARE RIGHT ");
+}
+function subans6()
+{
+    document.getElementById("subans6")
+    window.alert("HAPPINESS IS MORE IMPORTANT THAN MONEY");
+}
+function subans7()
+{
+    document.getElementById("subans7")
+    window.alert("YOUR TALENT IS HAPPY WITH YOU, REMEMBER THAT!");
+}
